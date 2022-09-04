1c452204a513be2f23514ad0bd937ba64eb6d2c4
Robi <robelmezemir@gmail.com>
2022-09-04 15:55:03 +0300

    styling done

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<!-- Header.svelte -->
+<footer>
+  <div>
+    <a href="https://github.com/robimez">A SvelteKit Hello world app</a>
+  </div>
+</footer>
+
+
+<style>
+  footer {
+    background: #1F1E1E;
+    color: #A7BC8A;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    justify-content: space-between;
+  }
+  footer div {
+    height: 100%;
+    padding:  1.3rem 2.3rem;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  a:hover {
+    color: #FF7070;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero">
+	<h1>Hi There.</h1>
+</div>
+
+<style>
+	div.hero {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	h1 {
+		background: radial-gradient(
+			circle,
+			#f48fb1 0%,
+			#ce93d8 20%,
+			#b2ebf2 40%,
+			#a5d6a7 60%,
+			#b39ddb 100%
+		);
+		-webkit-background-clip: text;
+		color: transparent;
+		background-size: 400%;
+		animation: gradient 10s linear infinite alternate;
+		font-size: 8rem;
+	}
+
+  h1:hover {
+		animation: gradient 4s linear infinite alternate;
+		font-size: 8rem;
+	}
+
+	@keyframes gradient {
+		0% {
+			background-position: 0%;
+		}
+		100% {
+			background-position: 100%;
+		}
+	}
+</style>
